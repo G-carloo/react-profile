2dcda57f2efb13c5aaef61691c15c6a0dfb111c6
Ganief <mgcarloo6@gmail.com>
2020-07-02 10:44:06 +0200

    Updates

---
+++ precache-manifest.994fd94cf61fcae85b69bff1d31a7141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe635aada82c995075e67e864703870",
+    "url": "/react-profile/index.html"
+  },
+  {
+    "revision": "dab0041705a053f53034",
+    "url": "/react-profile/static/css/main.afba6f3a.chunk.css"
+  },
+  {
+    "revision": "6ad83f1ece163ce99a15",
+    "url": "/react-profile/static/js/2.b1c4fc4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-profile/static/js/2.b1c4fc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab0041705a053f53034",
+    "url": "/react-profile/static/js/main.b416183a.chunk.js"
+  },
+  {
+    "revision": "8e605beefd26507e7404",
+    "url": "/react-profile/static/js/runtime-main.ac5ef451.js"
+  }
+]);
